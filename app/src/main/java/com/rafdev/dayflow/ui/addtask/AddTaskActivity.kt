755e4fc4b22dc961d246f629d251a6c5cfdcc78b
RafDes21 <israeltineo84@gmail.com>
2024-01-27 20:08:19 -0300

    add numbrepicker

--- app/src/main/java/com/rafdev/dayflow/ui/addtask/AddTaskActivity.kt
+++ app/src/main/java/com/rafdev/dayflow/ui/addtask/AddTaskActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.Toast
 import com.rafdev.dayflow.R
 import com.rafdev.dayflow.databinding.ActivityAddTaskBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,5 +24,28 @@ class AddTaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val hourPicker = findViewById<NumberPicker>(R.id.hourPicker)
+        val minutePicker = findViewById<NumberPicker>(R.id.minutePicker)
+
+
+        hourPicker.minValue = 1
+        hourPicker.maxValue = 24
+
+        minutePicker.minValue = 1
+        minutePicker.maxValue = 59
+
+        hourPicker.setFormatter { String.format("%02d", it) }
+        minutePicker.setFormatter { String.format("%02d", it) }
+        binding.btnSave.setOnClickListener {
+
+            val selectedHour = hourPicker.value
+            val selectedMinute = minutePicker.value
+
+            // Mostrar los datos en un Toast
+            val toastMessage = "Hora: $selectedHour:$selectedMinute\nDescripción"
+            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
