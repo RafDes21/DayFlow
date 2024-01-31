9714247a1828e1353f1faa51b1d9ac77db4d2e64
RafDes21 <israeltineo84@gmail.com>
2024-01-30 23:18:20 -0300

    add audio

--- app/src/main/java/com/rafdev/dayflow/ui/addtask/AddTaskActivity.kt
+++ app/src/main/java/com/rafdev/dayflow/ui/addtask/AddTaskActivity.kt
@@ -1,5 +1,6 @@
 package com.rafdev.dayflow.ui.addtask
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.media.MediaRecorder
@@ -9,12 +10,14 @@ import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.util.Log
+import android.widget.EditText
 import android.widget.NumberPicker
 import android.widget.Toast
 import com.rafdev.dayflow.R
 import com.rafdev.dayflow.databinding.ActivityAddTaskBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.File
+import java.util.Locale
 
 @AndroidEntryPoint
 class AddTaskActivity : AppCompatActivity() {
@@ -22,13 +25,9 @@ class AddTaskActivity : AppCompatActivity() {
         fun create(context: Context): Intent =
             Intent(context, AddTaskActivity::class.java)
 
-    }
+        const val REQ_CODE_SPEECH_INPUT = 100
 
-    private lateinit var mediaRecorder: MediaRecorder
-    private lateinit var speechRecognizer: SpeechRecognizer
-    private lateinit var recognizerIntent: Intent
-    private var isRecording = false
-    private lateinit var audioFile: File
+    }
 
     private lateinit var binding: ActivityAddTaskBinding
 
@@ -36,19 +35,6 @@ class AddTaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        mediaRecorder = MediaRecorder()
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-
-        binding.btnRecordVoice.setOnClickListener {
-            if (isRecording) {
-                stopRecording()
-            } else {
-                startRecording()
-            }
-        }
 
         val hourPicker = findViewById<NumberPicker>(R.id.hourPicker)
         val minutePicker = findViewById<NumberPicker>(R.id.minutePicker)
@@ -62,106 +48,53 @@ class AddTaskActivity : AppCompatActivity() {
         hourPicker.setFormatter { String.format("%02d", it) }
         minutePicker.setFormatter { String.format("%02d", it) }
 
-        binding.btnSave.setOnClickListener {
-            Log.i("audio", "audio prueba")
-            if (isRecording) {
-                Log.i("audio", "audio prueba pausa")
-
-                // Si está grabando, detener la grabación
-                stopRecording()
-            }
-
-            // Guardar la nota (descripción y hora)
-            saveNote()
+        binding.btnRecordVoice.setOnClickListener {
+            startVoiceInput()
         }
 
-        // Configurar el reconocimiento de voz
-        speechRecognizer.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(params: Bundle?) {}
+        binding.btnSave.setOnClickListener{
+            val textoIngresado = binding.editTextDescription.text.toString()
 
-            override fun onBeginningOfSpeech() {}
+            val selectedHour = hourPicker.value
+            val selectedMinute = minutePicker.value
 
-            override fun onRmsChanged(rmsdB: Float) {}
+            val mensaje = "Texto ingresado: $textoIngresado\nHora: ${String.format("%02d", selectedHour)}:${String.format("%02d", selectedMinute)}"
 
-            override fun onBufferReceived(buffer: ByteArray?) {}
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
 
-            override fun onEndOfSpeech() {}
+        }
 
-            override fun onError(error: Int) {}
+    }
 
-            override fun onResults(results: Bundle?) {
-                val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                if (!matches.isNullOrEmpty()) {
-                    val spokenText = matches[0]
-                    binding.editTextDescription.append(spokenText)
+    private fun startVoiceInput() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Escribe lo que deseas")
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT)
+        } catch (e: ActivityNotFoundException) {
+            // Manejar la excepción si no hay actividad disponible para el reconocimiento de voz
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQ_CODE_SPEECH_INPUT -> {
+                if (resultCode == RESULT_OK && data != null) {
+                    val result: ArrayList<String>? =
+                        data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    result?.get(0)?.let {
+                        val editTextDescription: EditText = findViewById(R.id.editTextDescription)
+                        editTextDescription.setText(it)
+                    }
                 }
             }
-
-            override fun onPartialResults(partialResults: Bundle?) {}
-
-            override fun onEvent(eventType: Int, params: Bundle?) {}
-        })
-    }
-
-    private fun startRecording() {
-        try {
-            mediaRecorder.apply {
-                reset()
-                setAudioSource(MediaRecorder.AudioSource.MIC)
-                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-
-                // Crear un archivo temporal para almacenar el audio
-                audioFile = getTempFile()
-
-                setOutputFile(audioFile.absolutePath)
-                prepare()
-                start()
-            }
-            isRecording = true
-            binding.btnRecordVoice.setImageResource(R.drawable.ic_alarm)
-
-            // Iniciar el reconocimiento de voz
-            speechRecognizer.startListening(recognizerIntent)
-        } catch (e: Exception) {
-            // Manejar errores al iniciar la grabación
-            e.printStackTrace()
         }
     }
 
-    private fun stopRecording() {
-        try {
-            mediaRecorder.stop()
-            mediaRecorder.release()
-            isRecording = false
-            binding.btnRecordVoice.setImageResource(R.drawable.ic_record_voice)
-
-            // Detener el reconocimiento de voz
-            speechRecognizer.stopListening()
-        } catch (e: Exception) {
-            // Manejar errores al detener la grabación
-            e.printStackTrace()
-        }
-    }
-
-    private fun getTempFile(): File {
-        val folder = File(filesDir, "audio_recordings")
-        if (!folder.exists()) {
-            folder.mkdirs()
-        }
-        return File(folder, "temp_audio.3gp")
-    }
-
-    private fun saveNote() {
-        val selectedHour = binding.hourPicker.value
-        val selectedMinute = binding.minutePicker.value
-        val description = binding.editTextDescription.text.toString()
-
-        // Ahora puedes utilizar 'audioFile' para trabajar con el archivo de audio
-        // (subir a un servidor, mover a un directorio permanente, etc.)
-
-        // Muestra los datos en un Toast
-        val toastMessage = "Hora: $selectedHour:$selectedMinute\nDescripción: $description"
-        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
-    }
 }
